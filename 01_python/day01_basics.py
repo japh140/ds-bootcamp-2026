61c9d7cf9c92c641fb0f8b0f12c6abaea276244b
Japhet Mindana <japh140@users.noreply.github.com>
2026-03-30 20:12:23 +0100

    Day 1: Python refresher scripts

---
+++ 01_python/day01_basics.py
@@ -0,0 +1,32 @@
+
+# Exercise 1: print and strings
+print("Japhet Frederick Mindana")
+print("London Enfield")
+print("junior data Scientist or bioinformatician")
+
+# Exercise 2: numbers and arithmetic
+x=12
+y=5
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+# Exercise 3: input and output
+fav_artist=input(" Who is your favourite artist")
+print("Hello", fav_artist)
+
+Name=input("What is your name ")
+print("Hello", Name)
+
+# Exercise 4: int conversion and simple calculation
+age=int(input("what is your age in years"))
+
+months=age*12
+print(" Age in months is ", months)
+weeks=age*52
+print("Age in weeks is ", weeks)
+days=age*365
+print("Age in days is ", days)
+
